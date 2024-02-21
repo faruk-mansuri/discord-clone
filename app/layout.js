622fad2d43a37d84e55094de00ce26a51ebcbebc
faruq <faruqmansuri707@gmail.com>
2024-02-21 14:59:52 +0530

    downgrade clerk

--- app/layout.js
+++ app/layout.js
@@ -16,9 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
